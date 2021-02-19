1eab790f0072ae7fb6489a97c313fe2b6e3faad6
Ankan-Ganguly <77388840+Ankan-Ganguly@users.noreply.github.com>
2021-02-19 14:43:37 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,26 @@
+class Chain{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.004,
+            length: 10
+        }
+        this.chain = Constraint.create(options);
+        this.pointB = pointB;
+        World.add(world, this.chain);
+    }
+    fly(){
+        this.chain.bodyA = null;
+    }
+    display(){
+        if(this.chain.bodyA){
+            var pointA = this.chain.bodyA.position;
+            var pointB = this.pointB;
+        strokeWeight(4);
+        stroke('blue');
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+}
